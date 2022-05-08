df72c42c832892c7c78399d6368e7543bcab0d72
js <99598950+jspoh@users.noreply.github.com>
2022-05-08 10:37:39 +0800

    Add files via upload

---
+++ indexscript.js
@@ -0,0 +1,27 @@
+const msg1 = 'Hello there!';
+const msg2 = 'Thanks for dropping by!';
+const h1 = document.querySelector('h1');
+let index = 0;
+
+const displayLoading = ()=>{
+    h1.classList.add('loading');
+}
+
+const displaymsg1 = () => {
+    if (index <= msg1.length) {
+        h1.innerText = msg1.slice(0, index++);
+        setTimeout(displaymsg1, 100);
+    }
+}
+
+const displaymsg2 = () => {
+    if (index <= msg2.length) {
+        h1.innerText = msg2.slice(0, index++);
+        setTimeout(displaymsg2, 100)
+    }
+}
+
+setTimeout(displaymsg1, 3000);
+setTimeout(()=>{h1.innerText=''; index=0; displaymsg2();}, 6000);
+setTimeout(()=>{h1.innerText=''; index=0;}, 10000);
+setTimeout(()=>{window.location.href='about.html';}, 11000);
